81e26b54465f3d81422417e80a039bd9f51494b8
matthieu <matthieu.caron@laposte.net>
2015-10-22 21:34:07 +0200

    q4 work

--- tp3.py
+++ tp3.py
@@ -11,6 +11,7 @@ Pour les tests fonction de (10,7,7,3) = 11
 '''
 
 import numpy as np
+import math
 
 def calculePrec(liste):
     '''
@@ -109,69 +110,67 @@ def naif(m,n,i,j) :
 
 def dynamic(m,n,i,j) :
     global tableau 
-    tableau = np.ndarray(shape=(m+1,n+1,i+1,j+1), dtype=int)
+    tableau = np.ndarray(shape=(m+1,n+1,i+1,j+1))
     for a in range(m+1) :
         for b in xrange(n+1):
             for c in xrange(i+1):
                 for d in xrange(j+1):
-                    tableau[a][b][c][d] = 0
+                    tableau[a][b][c][d] = None
+    for k in xrange(m+1):
+        tableau[k][1][0][0] = 1
+    for k in xrange(n+1):
+        tableau[1][k][0][0] = 1
+    tableau[1][1][0][0] = 0
     return dynamic_rec(m,n,i,j)
 
 
 def dynamic_rec(m,n,i,j):
-    if (m == n == 1) :
-        return 0
+    if not math.isnan(tableau[m][n][i][j]) :
+        return tableau[m][n][i][j]
     else :
         positif = []
         negatif = []
         #on coupe la gauche
-        for k in xrange(1,i) :
-            if tableau[m-k][n][i-k][j] != 0 :
-                res = tableau[m-k][n][i-k][j]
-            else :
-                res = dynamic_rec(m-k,n,i-k,j)
-            if(res > 0) :
+        for k in xrange(1,i+1):
+            res = (dynamic_rec(m-k,n,i-k,j))
+            if(res>0) :
                 positif.append(res)
             else :
                 negatif.append(res)
-        #on coupe la haut
-        for k in xrange(1,j) :
-            if tableau[m][n-k][i][j-k] != 0 :
-                res = tableau[m][n-k][i][j-k]
-            else :
-                res = dynamic_rec(m,n-k,i,j-k)
-            if(res > 0) :
+        #on coupe le haut
+        for k in xrange(1,j+1):
+            res = (dynamic_rec(m,n-k,i,j-k))
+            if(res>0) :
                 positif.append(res)
             else :
                 negatif.append(res)
         #on coupe la droite
-        for k in xrange(1,m-i) :
-            if tableau[m-k][n][i][j] != 0 :
-                res = tableau[m-k][n][i][j]
-            else :
-                res  = dynamic_rec(m-k,n,i,j)
-            if(res > 0) :
+        for k in xrange(1,m-i):
+            res = (dynamic_rec(m-k,n,i,j))
+            if(res>0) :
                 positif.append(res)
             else :
                 negatif.append(res)
-        #on coupe la bas
-        for k in xrange(1,n-j) :
-            if tableau[m][n-k][i][j] != 0 :
-                res = tableau[m][n-k][i][j]
-            else :
-                res = dynamic_rec(m,n-k,i,j)
-            if(res > 0) :
+        #on coupe le bas
+        for k in xrange(1,n-j):
+            res = (dynamic_rec(m,n-k,i,j))
+            if(res>0) :
                 positif.append(res)
             else :
                 negatif.append(res)
         if(negatif == []):
-            return -(max(positif)+1)
+            res = -(max(positif)+1)
+            tableau[m][n][i][j] = res
+            return res
         else :
-            return -(max(negatif))+1
+            res = -(max(negatif))+1
+            tableau[m][n][i][j] = res
+            return res
 
 #test
 #print naif(10,7,7,3)
 #print naif(10,7,5,3)
 # dynamic(10,7,7,3)
-print dynamic(3,2,0,2)
+#print dynamic(3,2,2,0)
 print dynamic(10,7,7,3)
+print dynamic(10,7,5,3)
