057f60d10d08668232cad23d82f0c2b971e74e15
matthieu <matthieu.caron@laposte.net>
2015-10-22 22:22:52 +0200

    dodo

--- tp3.py
+++ tp3.py
@@ -13,57 +13,6 @@ Pour les tests fonction de (10,7,7,3) = 11
 import numpy as np
 import math
 
-def calculePrec(liste):
-    '''
-        ici on veut connaitre le score du coup précédent comme dans la question 2
-    '''
-    val = liste[0]
-    print liste
-    print type(val)
-    for i in xrange(1,len(liste)):
-        iElement = liste[i]
-        if(val>0) :
-            if(iElement > val) :
-                val=iElement
-        elif (iElement <= 0) :
-            val=iElement
-        elif (val ==0) :
-            if (iElement < val) :
-                val = iElement
-        else :
-            if(iElement > val and iElement < 0) :
-                val = iElement
-        #Return a different value depending the min and max values
-        if (val == 0) :
-            return 1
-        elif (val > 0) :
-            return (-1*val - 1)
-        else :
-            return (-1*val + 1)
-        
-    
-
-# def naif(m,n,i,j) :
-#     barre = []
-#     # si qu'un seul carré
-#     if(m==n==1) :
-#         return 0
-#     #toutes les possibilités de découpages
-#     else :
-#         #on coupe la gauche
-#         for k in xrange(1,i+1):
-#             barre.append(naif(m-k,n,i-k,j))
-#         #on coupe le haut
-#         for k in xrange(1,j+1):
-#             barre.append(naif(m,n-k,i,j-k))
-#         #on coupe la droite
-#         for k in xrange(1,m-i):
-#             barre.append(naif(m-k,n,i,j))
-#         #on coupe le bas
-#         for k in xrange(1,n-j):
-#             barre.append(naif(m,n-k,i,j))
-#     return calculePrec(barre)
-
 
 def naif(m,n,i,j) :
     positif = []
@@ -121,6 +70,11 @@ def dynamic(m,n,i,j) :
         tableau[k][1][0][0] = 1
     for k in xrange(n+1):
         tableau[1][k][0][0] = 1
+    #superstition
+    for k in xrange(m+1):
+        tableau[k][0][0][0] = 1
+    for k in xrange(n+1):
+        tableau[0][k][0][0] = 1
     tableau[1][1][0][0] = 0
     return dynamic_rec(m,n,i,j)
 
